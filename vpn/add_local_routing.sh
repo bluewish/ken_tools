55604ffda6f029994bb46dcf20a3bcb996a8cd3e
Lu Ken <bluewish.ken.lu@gmail.com>
2014-11-24 00:59:58 +0800

    add more local route

--- vpn/add_local_routing.sh
+++ vpn/add_local_routing.sh
@@ -4,3 +4,9 @@ local_gw=$1
 
 # for github
 route add -net 192.30.252.0 netmask 255.255.255.0 gw $local_gw
+
+# for wx.qq.com
+route add -net 140.206.160.0 netmask 255.255.255.0 gw $local_gw
+
+# for music.163.com
+route add -net 123.58.180.0 netmask 255.255.255.0 gw $local_gw
